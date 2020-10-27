79253cb0cf932912baedc400f5eee2e7c7f804f4
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 14:45:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,11 +10,11 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  {:name => "Grace Hopper"}
+  # {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  {:id => number}
+  # {:id => number}
 end
